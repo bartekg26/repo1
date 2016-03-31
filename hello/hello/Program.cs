440dd3f3f55d22927ec8ada47d0fcc7ea9927a62
bartekg26 <bxaxrx12@wp.pl>
2016-03-31 10:19:52 +0200

    kolejny commit

--- hello/hello/Program.cs
+++ hello/hello/Program.cs
@@ -10,7 +10,7 @@ namespace hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("oks 10");
+            Console.WriteLine("oks 10qq");
         }
     }
 }
